8f19945d0427b1acdb533cc83d8ea17992f7dd41
aramendiander <aramendiander@gmail.com>
2023-10-20 00:43:13 +0200

    added last race full data, driver standings, constructor standings. Pending changing structure to css grid instead of using html tables.

--- scripts/index.js
+++ scripts/index.js
@@ -18,14 +18,14 @@ async function getFlags() {
 
 const flagData = getFlags()
 
-async function printRaceName(){
+async function getRaceName(){
         const data = await rawData;
         const raceInfo = data.MRData.RaceTable.Races[0].raceName;
         const h2 = document.getElementById("lastrace");
         h2.innerHTML = `Results for last race: ${raceInfo} 2023`;
 }
 
-async function getDriverFlag(country){
+async function getFlag(country){
     const data = await flagData;
     let flag = undefined;
     data.forEach(name => {
@@ -46,7 +46,7 @@ function printTd(arg){
     return td
 }
 
-async function printRaceResults() {
+async function getRaceResults() {
     const data = await rawData;
     const results = data.MRData.RaceTable.Races[0].Results;
     for (let i = 0; i < results.length; i++) {
@@ -67,21 +67,22 @@ async function printRaceResults() {
 
         // Print name cell
         const driverName = `${result.Driver.givenName} ${result.Driver.familyName}`;
+        const printDriverName = printTd(driverName);
         if (result.FastestLap?.rank) {
-            const raceFastestLap = result.FastestLap.rank
-            if(raceFastestLap === "1") {
-                tr.appendChild(printTd(driverName + '*'))
-            } else {
-                tr.appendChild(printTd(driverName))
+            const raceFastestLap = result.FastestLap.rank;
+            const fastestLapImage = document.createElement("img");
+            fastestLapImage.src = "./assets/fastestlap.png";
+            fastestLapImage.id = "fastestlap"
+            if (raceFastestLap === "1") {
+                printDriverName.appendChild(fastestLapImage);
             }
-        } else {
-            tr.appendChild(printTd(driverName))
         }
+        tr.appendChild(printDriverName);
         
 
         // Print nationality cell
         const driverNationality = result.Driver.nationality;
-        const driverFlag = await getDriverFlag(driverNationality);
+        const driverFlag = await getFlag(driverNationality);
         tr.appendChild(printTd(driverFlag))
 
         // Print constructor cell
@@ -102,9 +103,81 @@ async function printRaceResults() {
 }
 
 
+async function getDriverStandings() {
+    try {
+        const response = await fetch("https://ergast.com/api/f1/current/driverStandings.json");
+        const data = await response.json();
+        const results = data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+        for (let i = 0; i < results.length; i++) {
+            const result = results[i];
+            // Print rows
+            const container = document.getElementById("driverstandings");
+            const tr = document.createElement("tr");
+            container.appendChild(tr);
+
+            // Print standing position
+            const standingPosition = result.position;
+            tr.appendChild(printTd(standingPosition))
+
+            // Print standing driver
+            const standingDriver = `${result.Driver.givenName} ${result.Driver.familyName}`;
+            tr.appendChild(printTd(standingDriver))
+
+            // Print standing driver nationality
+            const driverNationality = result.Driver.nationality;
+            const driverFlag = await getFlag(driverNationality);
+            tr.appendChild(printTd(driverFlag))
+
+            // Print standing driver points
+            const driverPoints = result.points;
+            tr.appendChild(printTd(driverPoints))
+        }
+    } catch(error){
+        console.log(error);
+    }
+}
 
 
+async function getConstructorStandings() {
+    try {
+        const response = await fetch("https://ergast.com/api/f1/current/constructorStandings.json");
+        const data = await response.json();
+        const results = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings
+        for (let i = 0; i < results.length; i++) {
+            const result = results[i];
+            // Print rows
+            const container = document.getElementById("constructorstandings");
+            const tr = document.createElement("tr");
+            container.appendChild(tr);
 
-console.log(rawData)
-printRaceName();
-printRaceResults();
+            // Print standing position
+            const standingPosition = result.position;
+            tr.appendChild(printTd(standingPosition))
+
+            // Print standing constructor
+            const standingConstructor = result.Constructor.name ;
+            tr.appendChild(printTd(standingConstructor))
+
+            // Print standing driver nationality
+             const constructorCounty = result.Constructor.nationality;
+            const constructorFlag = await getFlag(constructorCounty);
+            tr.appendChild(printTd(constructorFlag))
+
+            // Print standing driver points
+            const constructorPoints = result.points;
+            tr.appendChild(printTd(constructorPoints)) 
+        }
+    } catch(error){
+        console.log(error);
+    }
+}
+
+
+function printIndex() {
+getRaceName();
+getRaceResults();
+getDriverStandings();
+getConstructorStandings();
+}
+
+printIndex()
